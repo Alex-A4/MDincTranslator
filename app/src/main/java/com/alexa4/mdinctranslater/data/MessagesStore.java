1af33a76c3b93c2ace89937150d6355d4615708a
Alex <alex.A4.25@yandex.ru>
2018-11-23 23:24:48 +0300

    Added methods to store info about languages

--- app/src/main/java/com/alexa4/mdinctranslater/data/MessagesStore.java
+++ app/src/main/java/com/alexa4/mdinctranslater/data/MessagesStore.java
@@ -2,6 +2,7 @@ package com.alexa4.mdinctranslater.data;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Singleton class to store messages of dialogue
@@ -14,11 +15,17 @@ public class MessagesStore {
     //List of messages
     private ArrayList<String> mMessages;
 
+    //Pairs of languages
+    private ArrayList<LanguageInfo> mPairs;
+
 
     //The language from which need translate
     private LanguageInfo mLanguageFrom;
     public LanguageInfo getLanguageFrom() {
         return mLanguageFrom;
+    }
+    public void setLanguageFrom(LanguageInfo languageFrom) {
+        mLanguageFrom = languageFrom;
     }
 
     //The language to which need translate
@@ -26,12 +33,58 @@ public class MessagesStore {
     public LanguageInfo getLanguageTo() {
         return mLanguageTo;
     }
+    public void setLanguageTo(LanguageInfo languageTo) {
+        mLanguageTo = languageTo;
+    }
 
     private MessagesStore() {
         mMessages = new ArrayList<>();
-        mLanguageFrom = new LanguageInfo("Русский", "ru");
-        mLanguageTo = new LanguageInfo("Английский", "en");
+        mPairs = new ArrayList<>();
+        fillList();
+        mLanguageFrom = mPairs.get(0);
+        mLanguageTo = mPairs.get(1);
     }
+
+    public ArrayList<LanguageInfo> getPairs() {
+        return mPairs;
+    }
+
+    private void fillList() {
+        mPairs.addAll(
+                Arrays.asList(
+                        new LanguageInfo("Русский", "ru"),
+                        new LanguageInfo("Английский", "en"),
+                        new LanguageInfo("Украинский", "uk"),
+                        new LanguageInfo("Японский", "ja"),
+                        new LanguageInfo("Малайсийский", "ms"),
+                        new LanguageInfo("Корейский", "ko"),
+                        new LanguageInfo("Китайский", "zh"),
+                        new LanguageInfo("Казахский", "kk"),
+                        new LanguageInfo("Испанский", "es"),
+                        new LanguageInfo("Итальянский", "it"),
+                        new LanguageInfo("Узбекский", "uz"),
+                        new LanguageInfo("Финский", "fi"),
+                        new LanguageInfo("Исландский", "is"),
+                        new LanguageInfo("Датский", "da"),
+                        new LanguageInfo("Французский", "fr"),
+                        new LanguageInfo("Шведский", "sv"),
+                        new LanguageInfo("Чешский", "cs"))
+        );
+    }
+
+    /**
+     * Getting language by its name
+     * @param name the name of language
+     * @return the language or default lang (russian)
+     */
+    public LanguageInfo getLangByName(String name) {
+        for (LanguageInfo info: mPairs)
+            if (name.equals(info.getLanguageName()))
+                return info;
+
+        return mPairs.get(0);
+    }
+
 
     public static MessagesStore getStore() {
         if (sStore == null)
